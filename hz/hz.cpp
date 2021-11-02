7ea153d0a487bd895eda80e99fb8e9db0339483f
Zero22813 <mmalyhin4@gmail.com>
2021-11-02 14:34:57 +0300

    edit

--- hz/hz.cpp
+++ hz/hz.cpp
@@ -4,5 +4,16 @@ using namespace std;
 
 int main(){
     cout << "hello world";
+    cout << "hello world";
+    cout << "hello world";
+    cout << "hello world";
+    cout << "hello world";
+    cout << "hello world";
+    cout << "hello world";
+    cout << "hello world";
+    cout << "hello world";
+    cout << "hello world";
+    cout << "hello world";
+    cout << "hello world";
     return 0;
 }
